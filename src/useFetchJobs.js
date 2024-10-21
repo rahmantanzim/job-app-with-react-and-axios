dbd0a1f25b527c5650bf309de83ef6c7573b14b0
Tanzim <tanzim008@gmail.com>
2024-10-21 02:54:42 -0230

    add reducer function, completed useFetchJob customer hook

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,12 +1,48 @@
-import React from 'react'
-const useFetchJobs = (params,page) => {
-  return (
-    {
-        jobs:[],
-        loading:true,
-        error:false
+import React, { useEffect, useReducer } from 'react'
+import axios from 'axios';
+// Define actions
+const ACTION = {
+    MAKE_REQUEST :'make_request',
+    GET_DATA: 'get_data',
+    ERROR: 'error'
+}
+const BASE_URL = 'https://remoteok.com/api';
+// Reducer function
+const reducer = (state,action) =>{ //reducer function is called everytime we call dispatch
+    switch(action.type){
+        case ACTION.MAKE_REQUEST:
+            return {
+                loading: true,
+                jobs: [],
+                error:''
+            }
+        case ACTION.GET_DATA:
+            return {...state,loading:false, jobs: action.payload.jobs}
+        case ACTION.ERROR:
+            return {...state,loading:false, error: action.payload.error,jobs:[]}
+        default: 
+            return state;
     }
-  )
+}
+const useFetchJobs = (params,page) => {
+    const initialState = {
+        jobs: [],
+        loading: false,
+        error: ''
+    }
+    const [state,dispatch] = useReducer(reducer,initialState);
+
+    useEffect(()=>{
+        dispatch({type:ACTION.MAKE_REQUEST});
+        axios.get(BASE_URL)
+        .then((response)=>{
+            dispatch({type: ACTION.GET_DATA , payload: {jobs: response.data}})
+        })
+        .catch((err)=>{
+            dispatch({ type: ACTION.ERROR, payload: { error: err.message || 'An error occurred' } });
+        })
+    },[params,page]);
+  return state;
 }
 
 export default useFetchJobs

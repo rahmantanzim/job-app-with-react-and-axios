98c327c4f5557c660e9b738082ed760c2f5c7526
Tanzim <tanzim008@gmail.com>
2024-10-21 02:01:05 -0230

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import useFetchJobs from './useFetchJobs';
 
 function App() {
+  const {jobs,loading,error} = useFetchJobs();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>ERROR!</h1>}
+      {<h1>{jobs.length}</h1>}
+    </>
   );
 }
 

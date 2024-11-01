4d2d1d43ce8296338719ec30a08c0f0dac94d855
Tanzim <tanzim008@gmail.com>
2024-11-01 20:52:43 -0230

    updated and completed pagination

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import useFetchJobs from "./useFetchJobs";
 import Job from "./Job";
@@ -7,30 +7,34 @@ import Row from "react-bootstrap/Row";
 import JobsPagination from "./JobsPagination";
 function App() {
   const [params, setParams] = useState({});
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(3);
+  const [hasNextPage,setHasnextPage]= useState(true)
   const { jobs, loading, error } = useFetchJobs(params, page);
   const totalJobs = jobs.length;
   const maxItemsPerPage = 10;
-  const itemsPerPage = Math.ceil(totalJobs/maxItemsPerPage);
+  const lastIndex = page*maxItemsPerPage;
+  const startIndex = lastIndex-maxItemsPerPage;
+  const currentPageJobs = jobs.slice(1).slice(startIndex, lastIndex);
+  const totalPages = Math.floor(totalJobs/maxItemsPerPage);
+
+  useEffect(()=>{
+    
+      setHasnextPage(page <=totalPages)
+    
+  },[page,totalPages])
   return (
     <>
       {loading && <h1>Loading...</h1>}
       {error && <h1>ERROR!</h1>}
       <Container>
         <h1 className="my-5">{jobs.length} jobs available</h1>
-        <JobsPagination page={page} setPage={setPage} />
+        <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
         <Row>
-          {jobs.map((job, index) => {
-            if (index === 0) {
-              return null;
-            }
-            if(index <= maxItemsPerPage){
-              return <Job key={job.id} job={job} />;  
-            }
-            
+          {currentPageJobs.map((job, index) => {
+            return <Job key={job.id} job={job} />;  
           })}
         </Row>
-        <JobsPagination page={page} setPage={setPage} />
+        <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       </Container>
     </>
   );
--- src/JobsPagination.jsx
+++ src/JobsPagination.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import {Pagination} from 'react-bootstrap';
-const JobsPagination = ({page,setPage}) => {
+const JobsPagination = ({page,setPage,hasNextPage}) => {
+  const onPageChange = (amount)=>{
+    setPage((prevPage)=>{
+      return prevPage+amount;
+    })
+  }
   return (
     <Pagination>
-        {page !== 1 && <Pagination.Prev/>}
-        {page !== 1 && <Pagination.Item>{page-1}</Pagination.Item>}
-        <Pagination.Item>{page}</Pagination.Item>
-        <Pagination.Item>{page+1}</Pagination.Item>
-        <Pagination.Next/>
+        {page !== 1 && <Pagination.Prev onClick={()=>{onPageChange(-1)}} />}
+        {page !== 1 && <Pagination.Item onClick={()=>{setPage(1)}} >{1}</Pagination.Item>}
+        {page> 2 && <Pagination.Ellipsis />}
+        {page> 2 && <Pagination.Item onClick={()=>{onPageChange(-1)}}>{page-1}</Pagination.Item>}
+
+        <Pagination.Item active>{page}</Pagination.Item>
+        {hasNextPage && <Pagination.Item onClick={()=>{onPageChange(1)}}>{page+1}</Pagination.Item>}
+        {hasNextPage && <Pagination.Next onClick={()=>{onPageChange(1)}}/>}
     </Pagination>
   )
 }
